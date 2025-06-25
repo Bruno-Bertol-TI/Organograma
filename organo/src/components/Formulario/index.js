3399b65e3d594e5f2fe19c79b6ab0a80e86263a0
Bruno-Bertol-TI <bbertol2312@outlook.com>
2025-06-25 13:20:58 -0300

    commit: TESTE REPOSITORIO.

---
+++ organo/src/components/Formulario/index.js
@@ -0,0 +1,38 @@
+import './Formulario.css'
+import CampoTexto from "../CampoTexto"
+import ListaSuspensa from '../ListaSuspenda'
+import Botao from '../Botao'
+
+const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (e) => {
+         e.preventDefault()
+    }
+    
+    return(
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo" />
+                <CampoTexto obrigatorio={true} label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
